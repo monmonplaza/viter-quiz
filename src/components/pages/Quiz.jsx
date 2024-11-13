a6413629e8e030fcc8a8fe96067aa7b8039d8198
uShire <afgregana@up.edu.ph>
2024-11-13 11:45:04 +0800

    123

--- src/components/pages/Quiz.jsx
+++ src/components/pages/Quiz.jsx
@@ -1,97 +1,221 @@
+import { PartyPopper } from 'lucide-react'
 import React from 'react'
 import { Link } from 'react-router-dom'
 
 const Quiz = () => {
     const [currentQuestion, setCurrentQuestion] = React.useState(0)
+    const [correctAnswer, setCorrectAnswer] = React.useState(0)
+    const [showResult, setShowResult] = React.useState(false)
+    const [isActive, setIsActive] = React.useState(null)
+    const [randomize, setRandomize] = React.useState(null)
+    const [selectedAnswer, setSelectedAnswer] = React.useState(null)
     const questions = [
         {
             id:1,
-            question: "Lorem ipsum dolor sit amet consectetur adipisicing eli?",
-            choice_1: "choiceq1a",
-            choice_2: "choiceq1b",
-            choice_3: "choiceq1c",
-            choice_4: "choiceq1d",
-            answer: 0
+            question: "q1Lorem ipsum dolor sit amet consectetur adipisicing eli?",
+            choices: [
+                {
+                    choice: "A. Lorem, ipsum dolor A", 
+                    isCorrect: true
+                },
+                {
+                    choice: "B. Lorem, ipsum dolor B", 
+                    isCorrect: false
+                },
+                {
+                    choice: "C. Lorem, ipsum dolor C", 
+                    isCorrect: false
+                },
+                {
+                    choice: "D. Lorem, ipsum dolor D", 
+                    isCorrect: false
+                },
+            ]
+          
         }, 
         {
             id:2,
-            question: "dolor sit amet consectetur adipisicing eli?",
-            choice_1: "choiceq2a",
-            choice_2: "choiceq2b",
-            choice_3: "choiceq2c",
-            choice_4: "choiceq2d",
-            answer: 2
+            question: "q2 dolor sit amet consectetur adipisicing eli?",
+            choices: [
+                {
+                    choice: "A. Q2 Lorem, ipsum dolor A", 
+                    isCorrect: false
+                },
+                {
+                    choice: "B. Q2Lorem, ipsum dolor B", 
+                    isCorrect: false
+                },
+                {
+                    choice: "C. Q2 Lorem, ipsum dolor C", 
+                    isCorrect: true
+                },
+                {
+                    choice: "D. Q2 Lorem, ipsum dolor D", 
+                    isCorrect: false
+                },
+            ]
+          
         },
-
         {
             id:3,
-            question: "adipisicing eli dolor sit amet consectetur ?",
-            choice_1: "choice1",
-            choice_2: "choice2",
-            choice_3: "choice3",
-            choice_4: "choice4",
-            answer: 3
+            question: "q3 adipisicing eli dolor sit amet consectetur ?",
+            choices: [
+                {
+                    choice: "A. Q3 Lorem, ipsum dolor A", 
+                    isCorrect: false
+                },
+                {
+                    choice: "B. Q3 Lorem, ipsum dolor B", 
+                    isCorrect: false
+                },
+                {
+                    choice: "C. Q3 Lorem, ipsum dolor C", 
+                    isCorrect: true
+                },
+                {
+                    choice: "D. Q3 Lorem, ipsum dolor D", 
+                    isCorrect: false
+                },
+            ]
+           
         },
-
         {
             id:4,
-            question: "consectetur adipisicing eli dolor sit amet  ?",
-            choice_1: "choice1",
-            choice_2: "choice2",
-            choice_3: "choice3",
-            choice_4: "choice4",
-            answer: 2
+            question: "q4 consectetur adipisicing eli dolor sit amet  ?",
+            choices: [
+                {
+                    choice: "A. Q4 Lorem, ipsum dolor A", 
+                    isCorrect: false
+                },
+                {
+                    choice: "B. Q4 Lorem, ipsum dolor B", 
+                    isCorrect: true
+                },
+                {
+                    choice: "C. Q4 Lorem, ipsum dolor C", 
+                    isCorrect: false
+                },
+                {
+                    choice: "D. Q4 Lorem, ipsum dolor D", 
+                    isCorrect: false
+                },
+            ]
         },
-
         {
             id:5,
-            question: "xxxconsectetur adipisicing eli dolor sit amet?",
-            choice_1: "choice1",
-            choice_2: "choice2",
-            choice_3: "choice3",
-            choice_4: "choice4",
-            answer: 2
+            question: "q5 xxxconsectetur adipisicing eli dolor sit amet?",
+            choices: [
+                {
+                    choice: "A. Q5 Lorem, ipsum dolor A", 
+                    isCorrect: false
+                },
+                {
+                    choice: "B. Q5 Lorem, ipsum dolor B", 
+                    isCorrect: true
+                },
+                {
+                    choice: "C. Q5 Lorem, ipsum dolor C", 
+                    isCorrect: false
+                },
+                {
+                    choice: "D. Q5 Lorem, ipsum dolor D", 
+                    isCorrect: false
+                },
+            ]
         },
     ]
 
 
+
+    React.useEffect(() => {
+        setRandomize (questions
+            .map(value => ({ value, sort: Math.random() }))
+            .sort((a, b) => a.sort - b.sort)
+            .map(({ value }) => value))
+    },[])
+
     const handleNextQuestion = () => {
-        setCurrentQuestion(prev => prev + 1)
-        console.log("length", questions.length - 1)
-        console.log("curr", currentQuestion)
+        setCurrentQuestion(prev => prev + 1);
+        setIsActive(null);
         if(currentQuestion  === questions.length - 1 ) {
-            setCurrentQuestion(0)
+            setShowResult(true)
         }
+        if (selectedAnswer.isCorrect === true) {
+            setCorrectAnswer(prev => prev + 1)
+           }
+
+    }
+
+    const handleChoiceClick = (item, key) => {
+       setIsActive(key)
+       setSelectedAnswer(item)
+    }
+
+    const handleRetake = () => {
+        setCurrentQuestion(0)
+        setShowResult(false)
+        setRandomize (questions
+            .map(value => ({ value, sort: Math.random() }))
+            .sort((a, b) => a.sort - b.sort)
+            .map(({ value }) => value))
     }
 
 
   return (
     <section className='bg-secondary min-h-screen relative'>
         <header className='p-2 py-4'>
-            <div className="grid-cols-10 grid gap-4 w-full">
-                {Array.from(Array(10).keys()).map((i) => (<div key={i} className='bg-accent h-2 rounded-md w-full'></div>))                
+            <div className={`flex gap-4 w-full` }>
+                {Array.from(Array(questions.length).keys()).map((i) => (<div key={i} className={`bg-accent h-2 rounded-md w-full ${i <= currentQuestion ? "opacity-100" : "opacity-50"}`}></div>))                
                 }
             </div>
         </header>
-        <div className="menu p-4">
-            <Link>Home</Link>
-        </div>
-
      
-        <div className={`absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 `} >
-            <p className='text-center text-warning'>Question {currentQuestion + 1}/{questions.length}</p>
-            <h5 className='mb-5'>{questions[currentQuestion].question}</h5>
-            <ul className='space-y-3 mb-10'>
-              
-                <li className='px-6 py-2 bg-slate-200 rounded-full'><button className='text-xs text-light w-full'>{questions[currentQuestion].choice_1}</button></li>
-                <li className='px-6 py-2 bg-slate-200 rounded-full'><button className='text-xs text-light w-full'>{questions[currentQuestion].choice_2}</button></li>
-                <li className='px-6 py-2 bg-slate-200 rounded-full'><button className='text-xs text-light w-full'>{questions[currentQuestion].choice_3}</button></li>
-                <li className='px-6 py-2 bg-slate-200 rounded-full'><button className='text-xs text-light w-full'>{questions[currentQuestion].choice_4}</button></li>
-                
-                
-            </ul>
-            <button className='btn btn-accent !rounded-full justify-center flex w-auto mx-auto' onClick={()=> handleNextQuestion()}>Next Question</button>
+
+        {showResult ? (
+        <div className={`absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 max-w-[400px] w-full text-center bg-primary p-5 py-10 rounded-md shadow-md`} >
+            
+            <div className='mb-5'>
+            {correctAnswer/questions.length * 100 > 51 ? (
+                <>
+                    <PartyPopper size={100} strokeWidth={1} className='text-success mx-auto mb-5' />
+                    <h4 className='mb-5'>Congratulation</h4>        
+                    
+                </>
+            ) : (
+                <>
+                    <h4 className='mb-5 text-alert'>Failed!</h4>        
+                   
+                </>
+            )}
+            </div>
+
+            
+
+            <h5 className='mb-3'>You got {Math.floor(correctAnswer / questions.length * 100)}%</h5>
+            <p className='text-base mb-5'>{correctAnswer}/{questions.length} Correct Answer</p>
+            {correctAnswer/questions.length * 100 > 51 ? (
+                <button className='btn btn-accent'>Print Certificate</button>
+            ) : (
+                <button onClick={handleRetake} className='btn btn-accent'>Retake Exam</button>
+                )
+            }
+            
         </div>
+        ) : (
+        <div className={`absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 max-w-[800px] flex flex-col items-center w-full bg-primary p-5 py-10 rounded-md shadow-md`} >
+            <p className='text-center '>Question {currentQuestion + 1}/{questions.length}</p>
+            <h5 className='mb-5'>{randomize !== null && randomize[currentQuestion].question}</h5>
+            <ul className='space-y-3 mb-10 max-w-[400px] w-full'>
+              {randomize !== null && randomize[currentQuestion].choices.map((item, key)=>(
+                <li className={` bg-slate-200 rounded-full w-full ${isActive === key ? "bg-alert bg-opacity-70 [&>*]:text-white" : ""}`} key={key}>
+                    <button className={`px-6 py-3 text-xs text-dark w-full`} onClick={() => handleChoiceClick(item, key)}>{item.choice}</button>
+                </li>
+              ))}
+            </ul>
+            <button className='btn btn-alert !rounded-full justify-center flex w-auto mx-auto' onClick={()=> handleNextQuestion()}>Next Question</button>
+        </div>
+        )} 
+        
      
         
 
